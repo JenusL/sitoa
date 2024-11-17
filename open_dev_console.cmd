1e1ae63fc4f9fcb89c28b49c3bfa6931dd033ffe
Jens Lindgren <lindgren_jens@hotmail.com>
2024-11-17 23:33:23 +0100

    use procedural user paths in open_dev_console

--- open_dev_console.cmd
+++ open_dev_console.cmd
@@ -3,8 +3,8 @@
 rem Windows helper script to set the environment and open a dev console by simply dubble-clicking the batch file.
 rem Update the paths below to the locations that is correct for your system.
 
-set "Path=C:\Users\lindg\DEV\Portable Python-2.7.17 x64\App\Python;%Path%"
-set "Path=C:\Users\lindg\DEV\SolidAngle\Arnold-7.3.5.0-windows\bin;%Path%"
+set "Path=%USERPROFILE%\DEV\Portable Python-2.7.17 x64\App\Python;%Path%"
+set "Path=%USERPROFILE%\DEV\SolidAngle\Arnold-7.3.5.0-windows\bin;%Path%"
 set "Path=%~dp0dist\Softimage_2015\Addons\SItoA\Application\Plugins\bin\nt-x86-64;%Path%" 
 
 set "ARNOLD_PLUGIN_PATH=%~dp0dist\Softimage_2015\Addons\SItoA\Application\Plugins\bin\nt-x86-64"
