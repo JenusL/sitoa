3bfd6d6031f23021571bc3559561e5ef8782116c
Jens Lindgren <lindgren_jens@hotmail.com>
2025-04-07 00:11:15 +0200

    update to arnold core 7.3.7.1

--- open_dev_console.cmd
+++ open_dev_console.cmd
@@ -4,7 +4,7 @@ rem Windows helper script to set the environment and open a dev console by simpl
 rem Update the paths below to the locations that is correct for your system.
 
 set "Path=%USERPROFILE%\DEV\Portable Python-2.7.17 x64\App\Python;%Path%"
-set "Path=%USERPROFILE%\DEV\SolidAngle\Arnold-7.3.6.0-windows\bin;%Path%"
+set "Path=%USERPROFILE%\DEV\SolidAngle\Arnold-7.3.7.1-windows\bin;%Path%"
 set "Path=%~dp0dist\Softimage_2015\Addons\SItoA\Application\Plugins\bin\nt-x86-64;%Path%" 
 
 set "ARNOLD_PLUGIN_PATH=%~dp0dist\Softimage_2015\Addons\SItoA\Application\Plugins\bin\nt-x86-64"
